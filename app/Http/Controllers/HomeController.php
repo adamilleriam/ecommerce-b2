0011c4056179bcdc7d98018acecb033bab359982
Trainer PNT <trainer@piit.local>
2019-07-26 10:21:19 +0600

    Featured product showcasing done

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -16,6 +16,7 @@ class HomeController extends Controller
     public function index()
     {
         $data['latest_products'] = Product::with(['category','brand'])->where('status','active')->orderBy('id','DESC')->limit(6)->get();
+        $data['featured_products'] = Product::with(['category','brand'])->where(['status'=>'active','is_featured'=>1])->orderBy('id','DESC')->limit(6)->get();
         return view('front.home',$data);
     }
 }
---
+++ resources/views/front/product/_list.blade.php
@@ -0,0 +1,29 @@
+<div class="col-6 col-md-4">
+    <div class="product">
+        <figure class="product-image-container">
+            <a href="{{ route('product.details',$product->id) }}" class="product-image">
+                <img src="{{ asset(isset($product->product_image[0])?$product->product_image[0]->file_path:'assets/frontend/images/products/no-image-available.png') }}" alt="product">
+            </a>
+            <a href="#" class="btn-quickview">Quick View</a>
+        </figure>
+        <div class="product-details">
+            <div class="ratings-container">
+                <div class="">
+                    <span class="" style="width:60%">{{ $product->category->name }}</span><!-- End .ratings -->
+                </div><!-- End .product-ratings -->
+            </div><!-- End .product-container -->
+            <h2 class="product-title">
+                <a href="{{ route('product.details',$product->id) }}">{{ $product->name }}</a>
+            </h2>
+            <div class="price-box">
+                <span class="product-price">{{ $product->price }}/-</span>
+            </div><!-- End .price-box -->
+
+            <div class="product-action">
+                <a href="product.html" class="paction add-cart" title="Add to Cart">
+                    <span>Add to Cart</span>
+                </a>
+            </div><!-- End .product-action -->
+        </div><!-- End .product-details -->
+    </div><!-- End .product -->
+</div>
